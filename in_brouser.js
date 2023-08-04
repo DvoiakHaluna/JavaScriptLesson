53dfab1ce844195ebdb7eb93483372b3839ad2c8
DvoiakHaluna <73938438+DvoiakHaluna@users.noreply.github.com>
2023-08-04 16:47:51 +0300

    Add files via upload

---
+++ in_brouser.js
@@ -0,0 +1,8 @@
+let answer1, answer2, answer3
+answer1 = prompt("Чи подобається вам літо","так/ні");
+answer2 = prompt("Чи любите ви кавунчики","так/ні");
+answer3 = prompt("Чи хочете ви, щоб літо тривало довше","так/ні");
+console.log("Ваші відповіді:")
+console.log("1:"+answer1)
+console.log("2:"+answer2)
+console.log("3:"+answer3)
